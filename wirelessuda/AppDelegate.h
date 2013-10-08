89d42c1ccab4c14317ac1495a9ae740c29260d35
ivanfee <ivanfee@ivanfeetekiMacBook-Pro.local>
2013-10-08 21:18:08 +0800

    Initial Commit

---
+++ wirelessuda/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  wirelessuda
+//
+//  Created by ivanfee on 13-10-8.
+//  Copyright (c) 2013年 苏州大学网络中心. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ wirelessuda/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  wirelessuda
+//
+//  Created by ivanfee on 13-10-8.
+//  Copyright (c) 2013年 苏州大学网络中心. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
