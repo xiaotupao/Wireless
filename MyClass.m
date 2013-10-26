40168e7d600eacf4b19c6763b46b64313c1caf4b
Tkey <45795934@qq.com>
2013-10-26 16:02:13 +0800

    init

---
+++ MyClass.m
@@ -0,0 +1,13 @@
+//
+//  MyClass.m
+//  wirelessuda
+//
+//  Created by Tkey on 13-10-26.
+//  Copyright (c) 2013年 苏州大学网络中心. All rights reserved.
+//
+
+#import "MyClass.h"
+
+@implementation MyClass
+
+@end
