388f83c9af1ad46739b474bb8667a85a32b73094
0yeziahehe <ivanfee@ivanfeetekiMacBook-Pro.local>
2013-11-10 19:07:30 +0800

    GateWay

---
+++ wirelessuda/Modules/GateWay/GateWayViewController.h
@@ -0,0 +1,13 @@
+//
+//  GateWayViewController.h
+//  wirelessuda
+//
+//  Created by ivanfee on 13-11-9.
+//  Copyright (c) 2013年 苏州大学信息化建设与管理中心. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GateWayViewController : UIViewController
+
+@end
---
+++ wirelessuda/Modules/GateWay/GateWayViewController.m
@@ -0,0 +1,38 @@
+//
+//  GateWayViewController.m
+//  wirelessuda
+//
+//  Created by ivanfee on 13-11-9.
+//  Copyright (c) 2013年 苏州大学信息化建设与管理中心. All rights reserved.
+//
+
+#import "GateWayViewController.h"
+
+@interface GateWayViewController ()
+
+@end
+
+@implementation GateWayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
